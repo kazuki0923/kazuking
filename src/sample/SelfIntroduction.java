ecc6eb2279f1577c5a76b171af2bdb31ad930b30
DL-Guest <DL-Guest@192.168.3.6>
2025-10-30 17:02:18 +0900

    SelfIntroduction.javaを追加

---
+++ src/sample/SelfIntroduction.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class SelfIntroduction {
+    public static void main(String[] args) {
+
+        String name = "木下一輝";
+        int age = 27;
+        String hobby = "サウナ";
+
+        System.out.println("私の名前は" + name + "です。");
+        System.out.println("年齢は" + age + "歳です。");
+        System.out.println("趣味は" + hobby + "です。");
+    }
+}
